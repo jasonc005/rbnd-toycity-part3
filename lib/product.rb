12c16af179b7282894cc377dbc0b6ef2be56737a
Jason <jason.chandlerp@gmail.com>
2016-04-26 20:50:12 -0400

    Add Product attributes price and stock, and in_stock? method

--- lib/product.rb
+++ lib/product.rb
@@ -1,23 +1,38 @@
 class Product
-  attr_reader :title
+  attr_reader :title, :price, :stock
 
   @@products = []
 
+  #Initialize Product instances
   def initialize(options={})
     @title = options[:title]
+    @price = options[:price]
+    @stock = options[:stock] || 0
     add_to_products
   end
 
+  #Returns array of all products
   def self.all
     @@products
   end
 
+  #Returns Product object by given title
   def self.find_by_title(to_find)
     @@products.find {|prod| prod.title == to_find}
   end
 
+  #Checks to see if a product is in stock
+  def in_stock?
+    if @stock <= 0
+      false
+    else
+      true
+    end
+  end
+
   private
 
+  #Used in initialize to make sure product doesn't already exist
   def add_to_products
     if @@products.map(&:title).include? @title
       raise DuplicateProductError, "#{@title} already exists."
